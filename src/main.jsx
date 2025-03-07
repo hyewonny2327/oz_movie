e1acb77372d2951d36e188fdc581c07cf911de3f
hyewonny2327 <wonny2327@gmail.com>
2025-03-07 13:49:22 +0900

    router분리

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MovieDetail from "./pages/MovieDetail.jsx";
-import Layout from "./pages/Layout.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
 import { SupabaseProvider } from "./supabase/index.js";
-import SignupPage from "./pages/SignupPage.jsx";
 import { UserProvider } from "./hooks/useProvider.jsx";
-import MyPage from "./pages/MyPage.jsx";
+import "./index.css";
+import AppRouter from "./routes/AppRouter.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <SupabaseProvider>
       <UserProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<App />}></Route>
-              <Route path="/details/:id" element={<MovieDetail />}></Route>
-              <Route path="/login" element={<LoginPage />}></Route>
-              <Route path="/signup" element={<SignupPage />}></Route>
-              <Route path="/mypage" element={<MyPage />}></Route>
-            </Route>
-          </Routes>
-        </Router>
+        <AppRouter></AppRouter>
       </UserProvider>
     </SupabaseProvider>
   </StrictMode>
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import MyPage from "./pages/MyPage.jsx";
+import App from "./App.jsx";
+import MovieDetail from "./pages/MovieDetail.jsx";
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />}></Route>
+          <Route path="/details/:id" element={<MovieDetail />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/signup" element={<SignupPage />}></Route>
+          <Route path="/mypage" element={<MyPage />}></Route>
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
